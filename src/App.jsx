fb9658def0db05ebd2d27646c5898e01f999d011
lmzacarias <luismiguelzacarias@outlook.com>
2021-08-21 23:31:22 -0500

    animaciones de svg

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import imgMain from "./octopus.png";
 import imgBanana from "./bananas.svg";
+import React, { useEffect, useState } from "react";
 
 import "./App.css";
 import "antd/dist/antd.css";
@@ -8,18 +9,26 @@ import "./index.css";
 import FirstScreen from "./components/FirstScreen";
 import BananaComponent from "./components/BananaComponent";
 
-function App() {
+const App = () => {
+  const [activated, setactivated] = useState(false);
+
+
+
   return (
     <div className="App">
-      <img src={imgMain} className="App-logo" alt="logo" />
-      <img src={imgBanana} className="App-logo" alt="logo" />
+      {/*  <img src={imgMain} className="App-logo" alt="logo" /> */}
+      {/*  <img src={imgBanana} className="App-logo" alt="logo" /> */}
       <BananaComponent />
-      <div className="text-white">
-       Pruebas
-      </div>
-      <FirstScreen />
+      <div className="text-white">{!activated?"1":"2"}</div>
+
+      <div></div>
+      <FirstScreen
+        setData={() => {
+          setactivated(true);
+        }}
+      />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/FirstScreen.jsx
+++ src/components/FirstScreen.jsx
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "antd";
 
-const FirstScreen = () => {
+const FirstScreen = ({setData}) => {
   const confetti = require("canvas-confetti");
 
   const myConfetti = () => {
+    setData();
     console.log("log_1:");
-    var myCanvas = document.createElement("canvas");
+     var myCanvas = document.createElement("canvas");
     myCanvas.className += " canva-css";
     document.body.appendChild(myCanvas);
 
@@ -19,7 +20,36 @@ const FirstScreen = () => {
       particleCount: 100,
       spread: 160,
     });
+   
+
+
+    var duration = 10 * 1000;
+    var animationEnd = Date.now() + duration;
+    var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+    function randomInRange(min, max) {
+      return Math.random() * (max - min) + min;
+    }
+
+    var interval = setInterval(function () {
+      var timeLeft = animationEnd - Date.now();
+
+      if (timeLeft <= 0) {
+        return clearInterval(interval);
+      }
+
+      var particleCount = 50 * (timeLeft / duration);
+      // since particles fall down, start a bit higher than random
+      myConfettiReady({
+        particleCount: 30,
+        spread: 160,
+      });
+     
+    }, 250);
   };
+
+
+ 
 
   return (
     <>
